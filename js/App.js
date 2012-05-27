48fb2889ceb60e73bd579c953004eccd27d2fdc7
Simon Goller <neosam@gmail.com>
2012-05-27 02:49:09 +0200

    Value is now accessable
     * Through get/setValue
     * Through callback function

--- js/App.js
+++ js/App.js
@@ -1,6 +1,9 @@
 define(['jquery', "neoforms"], function($, Neoforms){
     return function() {
         input = Neoforms.newInput();
+        input.setChangeFunction(function(value) {
+            console.log(value);
+        });
         console.log(input)
         $('body').append(input.getContainer());
     }
